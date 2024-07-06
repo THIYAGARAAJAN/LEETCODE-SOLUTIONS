cb03ec838b57c744ad92e7c8d01b2c67dd349c8e
THIYAGARAAJAN <166491082+THIYAGARAAJAN@users.noreply.github.com>
2024-07-06 21:44:11 +0530

    Time: 8 ms (49.79%), Space: 39.9 MB (80.9%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -5,13 +5,6 @@ class Solution {
         {
             return n;
         }
-        int a=0,b=1,c=0;
-        for(int i=2;i<=n;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
+        return fib(n-1)+fib(n-2);
     }
 }
