dd42fa0136c42f63fa15641fdb0d41a00e491aab
THIYAGARAAJAN <166491082+THIYAGARAAJAN@users.noreply.github.com>
2024-08-15 20:42:59 +0530

    Time: 6 ms (32.67%), Space: 41.7 MB (34.79%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.java
+++ 0778-reorganize-string/0778-reorganize-string.java
@@ -1,32 +1,75 @@
 class Solution {
     public String reorganizeString(String s) {
-        int a[] = new int[26];
+        HashMap<Character,Integer> map = new HashMap<Character, Integer>(); 
+
+        PriorityQueue<Character> pq = new PriorityQueue<Character>(new Comparator<Character>() {
+            public int compare(Character c1, Character c2)
+            {
+                return map.get(c2) - map.get(c1);
+            }
+        }); 
+               
         for(char c : s.toCharArray())
-            a[c-'a']++;
-        
-        int max = 0, letter=0;
-        for(int i=0;i<26;i++)
-            if(a[i]>max){
-                max = a[i];
-                letter = i;
-            }
-        if(max > (s.length()+1)/2) return "";
-        char res[] = new char[s.length()];
-        int index =0;
-
-        while(a[letter]-- > 0){
-            res[index]= (char)(letter+'a');
-            index+=2;
-        }
-        
-        for(int i=0;i<26;i++){
-            while(a[i]-- > 0){
-                if(index >= s.length()) index = 1;
-                res[index]= (char)(i+'a');
-                index+=2;
-            }
+        {
+            map.put(c, map.getOrDefault(c, 0) + 1); 
         }
 
-        return String.valueOf(res);
+        for(Character c : map.keySet())
+        {
+            pq.add(c);
+        }
+
+        StringBuilder sb = new StringBuilder(); 
+
+        while(!pq.isEmpty())
+        {
+            char a = pq.poll(); 
+            char b = 0;
+
+            if(!pq.isEmpty())
+            {
+                b = pq.poll();
+            }
+
+            if(b == 0)
+            {
+                if(!sb.isEmpty() && sb.charAt(sb.length()-1) == a)
+                {   
+                    return "";
+                }
+                else
+                {
+                    sb.append(a);
+                    map.put(a, map.get(a)-1);
+                }
+            }
+            else 
+            {
+                if(sb.length() > 0 && sb.charAt(sb.length()-1) == a)
+                {
+                    char temp = b;
+                    b = a;
+                    a = temp; 
+                }
+
+                sb.append(a); 
+                sb.append(b);
+
+                map.put(a, map.get(a)-1);
+                map.put(b, map.get(b)-1);
+            }
+
+            if(map.get(a) > 0)
+            {
+                pq.add(a);
+            }
+
+            if(map.getOrDefault(b,0) > 0)
+            {
+                pq.add(b);
+            }
+        }
+
+        return sb.toString();
     }
 }
