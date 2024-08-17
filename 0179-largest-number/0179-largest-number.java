f389b7bf39ba933e0babc91f80e1dcc4fcde23cd
THIYAGARAAJAN <166491082+THIYAGARAAJAN@users.noreply.github.com>
2024-08-17 11:54:01 +0530

    Time: 12 ms (8.44%), Space: 45.3 MB (5.32%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,72 +1,35 @@
-class Solution {
-    public void merge (int nums[],int si,int mid,int ei){
-        int arr[]=new int[ei-si+1];
-        int i=si;
-        int j=mid+1;
-        int k=0;
+class Solution 
+{
+    public String largestNumber(int[] nums) 
+    {
+      
+        String[] num = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            num[i] = String.valueOf(nums[i]);
 
-        while(i<=mid && j<=ei){
-            long m1=10;
-            while(m1<=nums[i]){
-                m1*=10;
+        for(int i = 0; i < num.length - 1; i++)
+        {
+            for(int j = i + 1; j < num.length; j++)
+            {
+                String s1 = num[i] + num[j];
+                String s2 = num[j] + num[i];
+                if(s2.compareTo(s1) > 0)
+                {
+                    String temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
             }
-            long m2=10;
-            while(m2<=nums[j]){
-                m2*=10;
-            }
-
-            long val1=nums[i]*m2+nums[j];
-            long val2=nums[j]*m1+nums[i];
-
-            if(val1>=val2){
-                arr[k]=nums[i];
-                i++;k++;
-            }
-            else{
-                arr[k]=nums[j];
-                j++;k++;
-            }
-
-        }
-        while(i<=mid){
-                arr[k]=nums[i];
-                i++;k++;
-            }
-            while(j<=ei){
-                arr[k]=nums[j];
-                j++;k++;
-            }
-
-            int q=si;
-            for(int z=0;z<arr.length;z++){
-                nums[q]=arr[z];
-                q++;
-            }
-    }
-    public void mergeSort(int nums[],int si,int ei){
-        if(si>=ei){
-            return;
-        }
-        int mid=si+((ei-si)/2);
-        mergeSort(nums,si,mid);
-        mergeSort(nums,mid+1,ei);
-        merge(nums,si,mid,ei);
-    }
-    public String largestNumber(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
-        StringBuilder sb=new StringBuilder("");
-        for(int i=0;i<nums.length;i++){
-            sb.append(nums[i]);
         }
 
-        if(nums[0]==0){
-            return"0";
-        }
+        if(num[0].equals("0"))
+        return "0";
 
-        else{
-            return sb.toString();
-        }
-        
-        
+        StringBuilder ans = new StringBuilder();
+        for(String i : num)
+        ans.append(i);
+
+
+        return ans.toString();
     }
 }
